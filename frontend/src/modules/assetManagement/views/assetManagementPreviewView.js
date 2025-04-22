20dfb8c2ba82067827ab2b1f5dd84f9a541a269c
Laura Hooper <laura.hooper@walkgrove.co.uk>
2025-04-22 15:21:36 +0100

    Add uses to asset preview pane - allow deletion/permanent deletion dependning on uses

--- frontend/src/modules/assetManagement/views/assetManagementPreviewView.js
+++ frontend/src/modules/assetManagement/views/assetManagementPreviewView.js
@@ -18,11 +18,19 @@ define(function(require){
       'click .asset-preview-edit-button': 'onEditButtonClicked',
       'click .asset-preview-delete-button': 'onDeleteButtonClicked',
       'click .asset-preview-restore-button': 'onRestoreButtonClicked',
-      'click .asset-preview-replace-button': 'onReplaceButtonClicked'
+      'click .asset-preview-replace-button': 'onReplaceButtonClicked',
+      'mouseover .asset-use-icon': 'onShowUseInfo',
+      'mouseout .asset-use-icon': 'onHideUseInfo'
     },
+
+    permanentDelete: false,
+    uses: 'none',
 
     preRender: function() {
       this.listenTo(this, 'remove', this.remove);
+
+      this.$('.asset-use-info').html("");
+      this.checkUsage();
     },
 
     selectAsset: function (event) {
@@ -30,6 +38,63 @@ define(function(require){
 
       var data = {eventToTrigger: 'assetModal:assetSelected', model: this.model};
       Origin.trigger('modal:passThrough', data);
+    },
+
+    checkUsage: async function() {
+      var self = this;
+      this.permanentDelete = false;
+
+      await $.ajax({
+        url: 'api/asset/uses/' + self.model.get('_id'),
+        type: 'GET',
+        success: (data) => {
+          try {
+            let parsed = JSON.parse(data);
+
+            let usageStr = ""; // + parsed.courses.length + ', components: ' + parsed.components.length;
+            parsed.courses.forEach(course => {
+              usageStr += "<br/><b>" + course.title + "</b><br/>";
+
+              parsed.components.forEach(component => {
+                if(component.courseId === course.id) usageStr += "* " + component.title + "<br/>";
+              });
+            });
+
+            console.log("✅ Parsed JSON:", parsed, parsed.courses.length, parsed.components.length, usageStr);
+            this.$('.asset-use-info').html("");
+
+            if(parsed.components.length === 0) {
+              this.permanentDelete = true;
+              this.$('.asset-use-info').addClass('is-visible');
+            } else {
+              this.model.set('usage', "");
+              this.uses = usageStr;
+              this.$('.asset-use-icon').addClass('is-visible');
+              this.$('.asset-use-info').addClass('hover');
+            }
+            this.model.set('usage', this.uses);
+            this.$('.asset-use-info').html(this.uses);
+
+          } catch (e) {
+            console.error("Failed to parse JSON:", e);
+          }
+
+        },
+        error: (data) => {
+          Origin.Notify.alert({
+            type: 'error',
+            text: Origin.l10n.t('app.errorrestoreasset', { message: data.message })
+          });
+        }
+      });
+    },
+
+    onShowUseInfo: function() {
+      this.$('.asset-use-info').addClass('is-visible');
+    },
+
+    onHideUseInfo: function() {
+      this.$('.asset-use-info').removeClass('is-visible');
     },
 
     onEditButtonClicked: function(event) {
@@ -56,7 +121,7 @@ define(function(require){
 
     onDeleteConfirmed: async function(confirmed) {
       var self = this;
-      var permanentDelete = false;
+      /*var permanentDelete = false;
 
       await $.ajax({
         url: 'api/asset/uses/' + self.model.get('_id'),
@@ -77,39 +142,39 @@ define(function(require){
             text: Origin.l10n.t('app.errorrestoreasset', { message: data.message })
           });
         }
-      });
+      });*/
 
-      console.log(permanentDelete);
+      console.log(this.permanentDelete);
       
-      // var _url = 'api/asset/trash/' + self.model.get('_id');
-      // if (permanentDelete === true) _url = 'api/asset/delete/' + self.model.get('_id');
+      var _url = 'api/asset/trash/' + self.model.get('_id');
+      if (this.permanentDelete === true) _url = 'api/asset/delete/' + self.model.get('_id');
 
-      // if (confirmed) {
-      //   $.ajax({
-      //     url: _url,
-      //     type: 'PUT',
-      //     success: function() {
-      //       if (Origin.permissions.hasPermissions(["*"])) {
-      //         self.model.set({_isDeleted: true});
-      //       } else {
-      //         self.model.trigger('destroy', self.model, self.model.collection);
-      //       }
-      //       if (permanentDelete === true) {
-      //         Origin.trigger('assetManagement:assetPreviewView:permanentdelete');
-      //       } else {
-      //           Origin.trigger('assetManagement:assetPreviewView:delete');
-      //       }
-      //       self.remove();
+      if (confirmed) {
+        $.ajax({
+          url: _url,
+          type: 'PUT',
+          success: function() {
+            if (Origin.permissions.hasPermissions(["*"])) {
+              self.model.set({_isDeleted: true});
+            } else {
+              self.model.trigger('destroy', self.model, self.model.collection);
+            }
+            if (this.permanentDelete === true) {
+              Origin.trigger('assetManagement:assetPreviewView:permanentdelete');
+            } else {
+              Origin.trigger('assetManagement:assetPreviewView:delete');
+            }
+            self.remove();
 
-      //        setTimeout(function() {
-      //         Origin.router.navigateTo('dashboard');
-      //         setTimeout(function() {
-      //           Origin.router.navigateTo('assetManagement');
-      //         }, 100);
-      //        }, 100);
-      //     }
-      //   });
-      // }
+             setTimeout(function() {
+              Origin.router.navigateTo('dashboard');
+              setTimeout(function() {
+                Origin.router.navigateTo('assetManagement');
+              }, 100);
+             }, 100);
+          }
+        });
+      }
     },
 
     onRestoreButtonClicked: function(event) {
